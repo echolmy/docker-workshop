b14c2c06cca2a157a707d4364fe5a4ba50e68e6f
echolmy <mengyu.liu.24@gmail.com>
2026-01-24 13:29:29 +0000

    implement bigquery with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 provider "google" {
-  project = "angular-box-485213-d6"
-  region  = "us-central1"
+  project = var.project
+  region  = var.region
 }
 
 resource "google_storage_bucket" "demo_bucket" {
-  name          = "angular-box-485213-d6-terraform-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +25,9 @@ resource "google_storage_bucket" "demo_bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
